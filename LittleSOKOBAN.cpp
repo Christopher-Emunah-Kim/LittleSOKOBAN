1f5fee30d5f73c7b2bd2825cd3b6f0bb78f49df7
Christopher <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-12-06 18:46:48 +0900

    <feat> add input process and default move

--- LittleSOKOBAN.cpp
+++ LittleSOKOBAN.cpp
@@ -21,20 +21,78 @@ int main()
         {1,1,1,1,1,1,1,1,1,1}
     };
 
+    int playerX;
+    int playerY;
+    int nextX;
+    int nextY;
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(playMap[i][j] == 2)
+            {
+                playerX = i;
+                playerY = j;
+            }
+        }
+    }
+    
     system("cls");
     cout << "SOKOBAN 게임에 오신것을 환영합니다\n";
     cout << "게임을 시작하시려면 아무키나 입력해주세요\n";
-    
 
     while(true)
     {
         if(_kbhit())
         {
+            //input
             char ch = _getch();
+            nextX = playerX;
+            nextY = playerY;
+            
+            if(ch == 'w' || ch == 'W')
+            {
+                nextX--;
+            }
+            else if(ch == 's' || ch == 'S')
+            {
+                nextX++;
+            }
+            else if(ch == 'a' || ch == 'A')
+            {
+                nextY--;
+            }
+            else if(ch == 'd' || ch == 'D')
+            {
+                nextY++;
+            }
+            else if(ch == 'q' || ch == 'Q')
+            {
+                exit(0);
+            }
             
             //Update
             {
-            
+                int nextPos = playMap[nextX][nextY];
+                //빈공간(0)이면 이동가능
+                if(nextPos == 0)
+                {
+                    playMap[nextX][nextY] = 2; //update player
+                    playMap[playerX][playerY] = 0;
+                    playerX = nextX;
+                    playerY = nextY;
+                }
+                //벽(1)이면 이동불가
+                else if(nextPos == 1)
+                {
+                    //nothing
+                }
+                //박스(3)이면 박스의 다음칸 확인
+                //박스 다음칸 빈공간이면 박스 밀어내기 가능
+                //박스 다음칸이 벽이면 이동 불가
+                //목표지점(4)이면 이동 후 클리어
+                
             }
             //Render
             {
@@ -49,19 +107,19 @@ int main()
                         switch(playMap[x][y])
                         {
                         case 0:
-                            cout << "  ";
+                            cout << "  "; //isle
                             break;
                         case 1:
-                            cout << "# ";
+                            cout << "# "; //wall
                             break;
                         case 2:
-                            cout << "@ ";
+                            cout << "@ "; //player
                             break;
                         case 3:
-                            cout << "B ";
+                            cout << "B "; //box
                             break;
                         case 4:
-                            cout << "$ ";
+                            cout << "$ "; //goal
                             break;
 
                         default:
@@ -73,7 +131,7 @@ int main()
                 }
             
                 cout << "====================================\n";
-                cout << "조작키 - W(upside) S(downside) A(lefside) D(rightside)" << endl;
+                cout << "조작키 - W(upside) S(downside) A(lefside) D(rightside) Q(QuitGame)" << endl;
             
             }
         }
