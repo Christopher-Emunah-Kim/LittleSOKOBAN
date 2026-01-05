746cf9e6406e47dfab4a11e310a514d10d8d2c3c
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-05 19:29:24 +0900

    <refactor> 렌더로직 분리, 불변구조체 적용시도

--- LittleSOKOBAN.cpp
+++ LittleSOKOBAN.cpp
@@ -1,173 +1,187 @@
 ﻿
+#include <array>
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
+#include <string>
 
 using namespace std;
 
-int main()
+struct Position
 {
-    int playMap[10][10] = 
-    {
-        {1,1,1,1,1,1,1,1,1,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,2,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,3,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,4,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1}
-    };
+    int x;
+    int y;
+};
 
-    int playerX;
-    int playerY;
-    int nextX;
-    int nextY;
+struct GameState
+{
+    array<array<int, 10>, 10> map;
+    Position playerPos;
+    bool bIsCleared;
+};
 
+
+Position FindPlayerPos(const array<array<int, 10>, 10>& map)
+{
     for(int i = 0; i < 10; i++)
     {
         for(int j = 0; j < 10; j++)
         {
-            if(playMap[i][j] == 2)
+            if(map[i][j] == 2)
             {
-                playerX = i;
-                playerY = j;
+                return {i, j};
             }
         }
     }
+    
+    return {-1, -1};
+}
+
+Position GetDirection(char input)
+{
+    switch(input)
+    {
+        case 'w':
+        case 'W':
+            {
+                return { -1, 0 };
+            }
+        break;
+        case 's':
+        case 'S':
+            {
+                return {1, 0};
+            }
+        break;
+        case 'a':
+        case 'A':
+            {
+                return { 0, -1 };
+            }
+        case 'd':
+        case 'D':
+            {
+                return { 0, 1 };
+            }
+        break;
+    default:
+        return { 0, 0 };
+    }
+}
+
+Position AddPosition(Position p1, Position p2)
+{
+    return { p1.x + p2.x, p1.y + p2.y};
+}
+
+
+void RenderGame(const GameState& gameState)
+{
+    system("cls");
+    cout << "SOKOBAN 게임에 오신것을 환영합니다\n";
+    cout << "====================================\n";
+
+    for(int x = 0; x < 10; x++)
+    {
+        for(int y = 0; y < 10; y++)
+        {
+            switch(gameState.map[x][y])
+            {
+            case 0:
+                cout << "  "; //isle
+                break;
+            case 1:
+                cout << "# "; //wall
+                break;
+            case 2:
+                cout << "@ "; //player
+                break;
+            case 3:
+                cout << "B "; //box
+                break;
+            case 4:
+                cout << "$ "; //goal
+                break;
+
+            default:
+                cout << "  ";
+                break;
+            }
+        }
+        cout << endl;
+    }
+            
+    cout << "====================================\n";
+    cout << "조작키 - W(upside) S(downside) A(lefside) D(rightside) Q(QuitGame)" << endl;
+}
+
+int main()
+{
+    GameState state = {
+        {{
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,2,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,3,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,4,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1,1}
+        }}
+        , FindPlayerPos(state.map)
+        , false
+    };
     
     system("cls");
     cout << "SOKOBAN 게임에 오신것을 환영합니다\n";
     cout << "게임을 시작하시려면 아무키나 입력해주세요\n";
 
-    bool bIsClear = false;
     
-    while(true)
+    while(!state.bIsCleared)
     {
-        if (bIsClear)
-            break;
-        
         if(_kbhit())
         {
             //input
-            char ch = _getch();
-            nextX = playerX;
-            nextY = playerY;
+            char input = _getch();
             
-            if(ch == 'w' || ch == 'W')
-            {
-                nextX--;
-            }
-            else if(ch == 's' || ch == 'S')
-            {
-                nextX++;
-            }
-            else if(ch == 'a' || ch == 'A')
-            {
-                nextY--;
-            }
-            else if(ch == 'd' || ch == 'D')
-            {
-                nextY++;
-            }
-            else if(ch == 'q' || ch == 'Q')
-            {
-                exit(0);
-            }
+            Position nextPos = AddPosition(state.playerPos, GetDirection(input));
+            int nextCell = state.map[nextPos.x][nextPos.y];
             
-            //Update
+            //빈공간(0)이면 이동가능
+            if(nextCell == 0)
             {
-                int nextPos = playMap[nextX][nextY];
-                //빈공간(0)이면 이동가능
-                if(nextPos == 0)
+                state.map[state.playerPos.x][state.playerPos.y] = 0;
+                state.map[nextPos.x][nextPos.y] = 2;
+                state.playerPos = nextPos;
+            }
+            //벽(1)이면 이동불가
+            else if(nextCell == 1)
+            {
+                //nothing
+            }
+            //박스(3)이면 박스의 다음칸 확인
+            else if (nextCell == 3)
+            {
+                Position boxNextPos = AddPosition(nextPos, GetDirection(input));
+                int boxNextCell = state.map[boxNextPos.x][boxNextPos.y];
+                
+                if (boxNextCell == 0 || boxNextCell == 4)
                 {
-                    playMap[nextX][nextY] = 2; //update player
-                    playMap[playerX][playerY] = 0;
-                    playerX = nextX;
-                    playerY = nextY;
-                }
-                //벽(1)이면 이동불가
-                else if(nextPos == 1)
-                {
-                    //nothing
-                }
-                //박스(3)이면 박스의 다음칸 확인
-                else if (nextPos == 3)
-                {
-                    //박스 다음칸 빈공간이면 박스 밀어내기 가능
-                    int dirX = nextX - playerX;
-                    int dirY = nextY - playerY;
-                    int boxNextX = nextX + dirX;
-                    int boxNextY = nextY + dirY;
-                    int boxNextPos = playMap[boxNextX][boxNextY];
+                    state.map[boxNextPos.x][boxNextPos.y] = 3;
+                    state.map[nextPos.x][nextPos.y] = 2;
+                    state.map[state.playerPos.x][state.playerPos.y] = 0;
+                    state.playerPos = nextPos;
                     
-                    if (boxNextPos == 0 || boxNextPos == 4)
+                    if (boxNextCell == 4)
                     {
-                        playMap[boxNextX][boxNextY] = 3; //box
-                        playMap[nextX][nextY] = 2; //player
-                        playMap[playerX][playerY] = 0; //isle
-                        
-                        playerX = nextX;
-                        playerY = nextY;
-                        
-                        //목표지점(4)이면 이동 후 클리어
-                        if (boxNextPos == 4)
-                        {
-                            bIsClear = true;
-                        }
+                        state.bIsCleared = true;
                     }
-                    //박스 다음칸이 벽이면 이동 불가
-                    else if (boxNextPos == 1)
-                    {
-                        //nothing
-                    }
-                    
-                    
                 }
-                //목표지점(4)이면 이동 후 복원
             }
-            //Render
-            {
-                system("cls");
-                cout << "SOKOBAN 게임에 오신것을 환영합니다\n";
-                cout << "====================================\n";
-
-                for(int x = 0; x < 10; x++)
-                {
-                    for(int y = 0; y < 10; y++)
-                    {
-                        switch(playMap[x][y])
-                        {
-                        case 0:
-                            cout << "  "; //isle
-                            break;
-                        case 1:
-                            cout << "# "; //wall
-                            break;
-                        case 2:
-                            cout << "@ "; //player
-                            break;
-                        case 3:
-                            cout << "B "; //box
-                            break;
-                        case 4:
-                            cout << "$ "; //goal
-                            break;
-
-                        default:
-                            cout << "  ";
-                            break;
-                        }
-                    }
-                    cout << endl;
-                }
             
-                cout << "====================================\n";
-                cout << "조작키 - W(upside) S(downside) A(lefside) D(rightside) Q(QuitGame)" << endl;
-            
-            }
+            RenderGame(state);
         }
     }
     
