c1a662e2afbb0deb36d98a37cc1d80dd1f39e2e1
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-12 11:55:53 +0900

    <feat> Render Player

--- NewLittleSOKOBAN.cpp
+++ NewLittleSOKOBAN.cpp
@@ -1,10 +1,21 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
-{
-    cout << "새로운 시작" << endl;
+{ 
+    int playerX = 5;
+    int playerY = 5;
+    
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD playerPos = { (SHORT)playerX, (SHORT)playerY };
+    
+    SetConsoleCursorPosition(hConsole, playerPos);
+    
+    cout << "@" << endl;
+    
+    
     
     return 0;
 }
