079551b4c3cb09b1c9f5ba125a19925cb65f7ed1
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-12 20:19:28 +0900

    <feat> Player Moving

--- NewLittleSOKOBAN.cpp
+++ NewLittleSOKOBAN.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -8,14 +9,42 @@ int main()
     int playerX = 5;
     int playerY = 5;
     
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD playerPos = { (SHORT)playerX, (SHORT)playerY };
     
-    SetConsoleCursorPosition(hConsole, playerPos);
+    while (true)
+    {
+        system("cls");
+        
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD playerPos = { (SHORT)playerX, (SHORT)playerY };
     
-    cout << "@" << endl;
+        SetConsoleCursorPosition(hConsole, playerPos);
+    
+        cout << "@" << endl;
+    
+        char input = _getch();
+    
+        switch (input)
+        {
+        case 'w':
+        case 'W':
+            {
+                playerY--;
+            }
+            break;
+        case 'q':
+        case 'Q':
+            {
+                cout << "게임이 종료되었습니다" << endl;
+                return 0;
+            }
+            break;
+        default:
+        {
+            cout << "올바른 입력이 아닙니다" << endl;
+        }
+            break;
+        }
+    }
     
     
-    
-    return 0;
 }
