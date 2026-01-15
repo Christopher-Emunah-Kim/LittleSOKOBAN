190ce9d27e37eab4dbdb389ce58faf8343e7f9cf
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-15 13:38:30 +0900

    <feat> 벽하나 플레이어하나. WASD이동

--- NewLittleSOKOBAN.cpp
+++ NewLittleSOKOBAN.cpp
@@ -7,19 +7,24 @@ using namespace std;
 int main()
 { 
     int playerX = 5;
-    int playerY = 5;
+    int playerY = 7;
     
+	int wallX = 5;
+	int wallY = 3;
     
     while (true)
     {
         system("cls");
         
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        
         COORD playerPos = { (SHORT)playerX, (SHORT)playerY };
-    
         SetConsoleCursorPosition(hConsole, playerPos);
-    
-        cout << "@" << endl;
+        cout << "@";
+        
+        COORD wallPos = { (SHORT)wallX, (SHORT)wallY };
+        SetConsoleCursorPosition(hConsole, wallPos);
+        cout << "#";
     
         char input = _getch();
     
@@ -28,7 +33,42 @@ int main()
         case 'w':
         case 'W':
             {
-                playerY--;
+                int nextY = playerY - 1;
+                if (nextY != wallY || playerX != wallX)
+                {
+                    playerY = nextY;
+                }
+                
+            }
+            break;
+        case 's':
+        case 'S':
+            {
+                int nextY = playerY + 1;
+                if (nextY != wallY || playerX != wallX)
+                {
+                    playerY = nextY;
+                }
+            }
+            break;
+        case 'a':
+        case 'A':
+            {
+                int nextX = playerX - 1;
+                if (nextX != wallX || playerY != wallY)
+                {
+                    playerX = nextX;
+                }
+            }
+            break;
+        case 'd':
+        case 'D':
+            {
+                int nextX = playerX + 1;
+                if (nextX != wallX || playerY != wallY)
+                {
+                    playerX = nextX;
+                }
             }
             break;
         case 'q':
@@ -38,6 +78,7 @@ int main()
                 return 0;
             }
             break;
+            
         default:
         {
             cout << "올바른 입력이 아닙니다" << endl;
