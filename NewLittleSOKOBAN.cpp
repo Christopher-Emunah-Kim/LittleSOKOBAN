332adee3e0dc9dc2fd05ce87443ffa35128b25ef
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-15 15:49:28 +0900

    <refact> 골 배열 바꾸고 클리어 판정 함수 추출

--- NewLittleSOKOBAN.cpp
+++ NewLittleSOKOBAN.cpp
@@ -5,10 +5,16 @@
 using namespace std;
 
 constexpr int WALL_COUNT = 3;
-int wallX[3] = {5, 5, 4};
-int wallY[3] = {3, 4, 4};
+const int wallX[3] = {5, 5, 4};
+const int wallY[3] = {3, 4, 4};
+
+constexpr int GOAL_COUNT = 3;
+const int goalX[3] = {8, 10, 9};
+const int goalY[3] = {8, 10, 9};
+
 
 bool IsWallAt(int InX, int InY);
+bool IsBoxAtGoal(int InX, int InY);
 
 int main()
 { 
@@ -17,15 +23,6 @@ int main()
     
     int boxX = 3;
     int boxY = 3;
-    
-    int goal1X = 8;
-    int goal1Y = 8;
-    
-    int goal2X = 10;
-    int goal2Y = 10;
-    
-    int goal3X = 9;
-    int goal3Y = 9;
     
     string clearMsg = "게임이 클리어되었습니다!";
     
@@ -41,15 +38,6 @@ int main()
         COORD boxPos = { (SHORT)boxX, (SHORT)boxY };
         SetConsoleCursorPosition(hConsole, boxPos);
         cout << "B";
-        COORD goal1Pos = { (SHORT)goal1X, (SHORT)goal1Y };
-        SetConsoleCursorPosition(hConsole, goal1Pos);
-        cout << "G";
-        COORD goal2Pos = { (SHORT)goal2X, (SHORT)goal2Y };
-        SetConsoleCursorPosition(hConsole, goal2Pos);
-        cout << "G";
-        COORD goal3Pos = { (SHORT)goal3X, (SHORT)goal3Y };
-        SetConsoleCursorPosition(hConsole, goal3Pos);
-        cout << "G";
         
         for (int i = 0; i < WALL_COUNT; i++)
         {
@@ -58,11 +46,17 @@ int main()
             cout << "#";
         }
         
+        for (int i = 0; i < GOAL_COUNT; i++)
+        {
+            COORD goalPos = {(SHORT)goalX[i], (SHORT)goalY[i]};
+            SetConsoleCursorPosition(hConsole, goalPos);
+            cout << "G";
+        }
+        
         cout.flush();
         
-        if ((boxX == goal1X && boxY == goal1Y)
-            || (boxX == goal2X && boxY == goal2Y)
-            || (boxX == goal3X && boxY == goal3Y))
+        //game clear check
+        if (IsBoxAtGoal(boxX, boxY))
         {
             cout<< "\n\n\n\n\n";
             cout << clearMsg << endl;
@@ -191,11 +185,23 @@ bool IsWallAt(int InX, int InY)
 {
     for (int i = 0; i < WALL_COUNT; i++)
     {
-        if (InY == wallY[i] && InX == wallX[i])
+        if (InX == wallX[i] && InY == wallY[i])
         {
             return true;
         }
     }
     
+    return false;
+}
+
+bool IsBoxAtGoal(int InX, int InY)
+{
+    for (int i = 0; i < GOAL_COUNT; i++)
+    {
+        if (InX == goalX[i] && InY == goalY[i])
+        {
+            return true;
+        }
+    }
     return false;
 }
